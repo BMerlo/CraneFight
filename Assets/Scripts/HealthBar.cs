41f8083a1d567cdd6d5b739b9c16c0fd3db5eb84
Dan <browning.dan@gmail.com>
2018-10-10 13:26:37 -0400

    added semblance of healthbar

---
+++ Assets/Scripts/HealthBar.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBar : MonoBehaviour {
+
+    public int myHealth;
+    public int currentHealth;
+    public GameObject healthBar;
+
+	// Use this for initialization
+	void Start ()
+    {
+        myHealth = 50;
+        currentHealth = 50;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        
+        if (myHealth != currentHealth)
+        {
+            healthBar.transform.localScale = new Vector3(myHealth, healthBar.transform.localScale.y, healthBar.transform.localScale.z);
+            myHealth = currentHealth;
+        }
+
+    }
+    
+    public void SetHealthBar(int myHealth)
+    {
+        healthBar.transform.localScale = new Vector3(Mathf.Clamp(myHealth, 0f, 1f), healthBar.transform.localScale.y, healthBar.transform.localScale.z);
+    }
+}
+
