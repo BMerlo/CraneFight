bc387a4f7b42e0e7a0b016e475c6fa3f0bbdb1d6
Dan <browning.dan@gmail.com>
2018-10-10 19:04:54 -0400

    Coloured health bars, finished(?) win panels

--- Assets/Scripts/Game_Manager.cs
+++ Assets/Scripts/Game_Manager.cs
@@ -18,20 +18,22 @@ public class Game_Manager : MonoBehaviour {
     // Use this for initialization
     void Start ()
     {
-        
-        
+        Time.timeScale = 1;
+
         //numberOfPlayers = player1, player2, player3, player4;
-	}
+    }
 	
 	// Update is called once per frame
 	void Update ()
     {
 		if (numberOfPlayers.Length == 1)
         {
+            Time.timeScale = 0;
             winPanel.SetActive(true);
         }
         else if (numberOfPlayers.Length == 0)
         {
+            Time.timeScale = 0;
             tiePanel.SetActive(true);
         }
     }
