2a46c3e4d911850cc41f103864da1cd1eca7ba45
Erdem Gunay <merdemgunay88@gmail.com>
2018-10-16 16:42:05 -0400

    Falling off

---
+++ Assets/fallOff.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fallOff : MonoBehaviour {
+    [SerializeField] float maxY = 3.51f;
+    [SerializeField] float minY = -3.91f;
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log(this.transform.position.x);
+        if (this.transform.position.y > maxY || this.transform.position.y < minY)
+        {
+            fall();
+        }
+	}
+
+
+    void fall()
+    {
+        Debug.Log("Falling!");
+        if (GetComponent<playerController>())
+        {
+            GetComponent<playerController>().enabled = false;
+        }
+        if (GetComponent<BoxCollider2D>())
+        {
+            GetComponent<BoxCollider2D>().enabled = false;
+        }
+        if (GetComponent<CircleCollider2D>())
+        {
+            GetComponent<CircleCollider2D>().enabled = false;
+        }
+
+        if (GetComponent<Rigidbody2D>())
+        {
+            GetComponent<Rigidbody2D>().gravityScale = 5;
+        }
+
+        GetComponent<SpriteRenderer>().sortingOrder = -3;
+        GetComponent<SpriteRenderer>().sortingLayerName = "Background";
+
+        Destroy(this.gameObject, 1.5f);
+    }
+}
