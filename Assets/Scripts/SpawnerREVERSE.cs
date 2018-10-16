60592a75bf1cb36d1ba90919ea1dd0047c4c2db7
SJH607-18W\Graphic  milton <msparisi@hotmail.com>
2018-10-16 16:17:46 -0400

    Forgot to push these before for the reversing

--- Assets/Scripts/SpawnerREVERSE.cs
+++ Assets/Scripts/SpawnerREVERSE.cs
@@ -2,15 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpawnerREVERSE : MonoBehaviour {
+public class SpawnerREVERSE : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    [SerializeField] GameObject[] spawnees;
+    // [SerializeField] GameObject spawnee;
+    [SerializeField] float minTime = 2;
+    [SerializeField] float maxTime = 6;
+    float counter = 0;
+    float timeActual;
+
+    // Use this for initialization
+    void Start()
+    {
+        timeActual = Random.Range(minTime, maxTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        counter += Time.deltaTime;
+
+        if (counter >= timeActual)
+        {
+            counter = 0;
+            timeActual = Random.Range(minTime, maxTime);
+
+            int i = Random.Range(0, spawnees.Length);
+
+            Instantiate(spawnees[i], this.transform.position, this.transform.rotation);
+
+        }
+
+
+
+    }
 }
