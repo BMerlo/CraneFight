f4a6d177882e692da4f26e69c40c6dfa79c3f22b
Fhaor <bodamelo@gmail.com>
2019-01-23 02:08:47 -0500

    Kraken and tentacles added
    Just placeholders but prefabs and scripts working
    - kraken checks if it's at the top or bottom, and according to that they spawn tentacles between every 1.5 and 10 seconds. That value gets reseted once the kraken spawns something.
    - tentacles destroy AICars and get destroyed once their position in the X axis is lower than -10.

---
+++ Assets/Scripts/Kraken.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Kraken : MonoBehaviour {
+    [SerializeField] GameObject tentacle;    
+    [SerializeField] float minTime = 1.5f;
+    [SerializeField] float maxTime = 10.5f;
+    public float randomTime;
+    float counter = 0;
+    public bool top;
+
+    // Use this for initialization
+    void Start () {
+        randomTime = Random.Range(minTime, maxTime);
+        Debug.Log(transform.position);
+
+        if (transform.position.y < 0)
+        {
+            top = false;
+        }
+        else
+        {
+            top = true;
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        counter += Time.deltaTime;
+
+        if (counter >= randomTime && !top)
+        {
+            counter = 0;
+            
+            Instantiate(tentacle, transform.position + (tentacle.transform.up * -0.5f), transform.rotation);
+            randomTime = Random.Range(minTime, maxTime);
+        }
+        else if (counter >= randomTime && top)
+        {            
+            counter = 0;            
+            Instantiate(tentacle, transform.position + (tentacle.transform.up * 1), transform.rotation);
+            randomTime = Random.Range(minTime, maxTime);
+        }
+
+    }
+}
---
+++ Assets/Scripts/Tentacle.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tentacle : MonoBehaviour {
+    const float speed = 0.05f;
+    Vector3 dir = new Vector3(-1, 0, 0);
+    public bool reverse;
+    // Use this for initialization
+    void Start () {
+        if (transform.position.y < 0)
+        {
+            reverse = true;
+        }
+        else {
+            reverse = false;
+        }
+	}
+    
+    void Update() {
+
+        if (reverse) { 
+        this.transform.Translate(dir * speed);
+        }
+        else { 
+        this.transform.Translate(-dir * speed);
+        }
+
+        if (transform.position.x < -10)
+            Destroy(gameObject);
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.transform.tag == "AICar") {
+            Destroy(collision.gameObject);
+        }
+    }
+
+}
