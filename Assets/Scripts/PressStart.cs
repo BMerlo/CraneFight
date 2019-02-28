bcfdc4e04dd97d3a86162247ccb1267d7d7570a3
Fhaor <bodamelo@gmail.com>
2019-02-27 23:10:06 -0500

    new start menu
    2 new scripts, PressStart and Ready.
    PressStart just checks if players are ready and then
    changes the scene.
    Ready is just to separete who pressed what and by that
    changes the color of the UI.
    1 new scene.
    StartMenu it's just a canvas and 4 buttons that change colors when players press A.

---
+++ Assets/Scripts/PressStart.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PressStart : MonoBehaviour
+{
+    public float delayToLoadScene = 3;
+        
+    public bool P1Ready, P2Ready, P3Ready, P4Ready;
+       
+    // Start is called before the first frame update
+    void Start()
+    {
+        P1Ready = false;
+        P2Ready = false;
+        P3Ready = true;//I don't have 4 controllers x)
+        P4Ready = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (P1Ready && P2Ready/*&&P3Ready&&P4Ready*/)
+        {
+            Invoke("startGame", delayToLoadScene); 
+        }
+    }
+
+    void startGame()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+
+}
