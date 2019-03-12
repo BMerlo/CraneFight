6d06c4026a9ad575896a686408b55ff3c3b35154
daimenion <daimenion@hotmail.com>
2019-03-11 21:59:08 -0400

    added fish spawner and fish physics
    added states in editor and changed fish prefab

---
+++ Assets/Scripts/FishJump.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishJump : MonoBehaviour
+{
+    Rigidbody2D fishRigidbody;
+    Animator fishAnimation;//animation
+    float minForce = 40.0f;
+    float maxForce = 140.0f;
+    float randomForce;
+    bool Jumping = false;// if it does not stop yet it should go straight up 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        randomForce = Random.Range(minForce, maxForce);
+        fishAnimation = this.GetComponent<Animator>();
+        fishRigidbody = this.GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("speed  " + fishRigidbody.velocity.magnitude);
+        FishUP();
+
+      
+    }
+
+    void FishUP()
+    {
+        if (!Jumping)
+        {
+            fishRigidbody.AddForce(Vector2.up* randomForce);//add force to push upwards
+           
+        }
+        if (fishRigidbody.velocity.magnitude < 0.3&&!Jumping)// if the speed is less than 0.3 then it should stop
+        {
+            Jumping = true;
+            fishAnimation.SetBool("Landed", Jumping);//when landed play turn and wiggle animation 
+            fishRigidbody.velocity = new Vector2(0, 0);//set velocity to 0;
+        }
+
+    }
+}
---
+++ Assets/Scripts/FishSpawnerV2.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishSpawnerV2 : MonoBehaviour
+{
+    [SerializeField] GameObject m_Fish;
+    // [SerializeField] GameObject spawnee;
+    [SerializeField] float minTime = 2;
+    [SerializeField] float maxTime = 6;
+    float counter = 0;
+    float timeActual;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        timeActual = Random.Range(minTime, maxTime);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        counter += Time.deltaTime;
+
+
+        SpawnChecked();
+
+
+    }
+
+
+    void SpawnChecked()
+    {
+        if (counter >= timeActual)
+        {
+            Instantiate(m_Fish, this.transform.position, this.transform.rotation);
+
+            counter = 0;
+            timeActual = Random.Range(minTime, maxTime);
+        }
+    }
+
+    //fish jump on to the road
+
+}
