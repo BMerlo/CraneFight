75f842d742d0d24fb5902290b9e4221f3a5d8486
Erdem Gunay <merdemgunay88@gmail.com>
2019-04-03 21:08:02 -0400

    point system implementation

--- Assets/pointSystem.cs
+++ Assets/pointSystem.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class pointSystem : MonoBehaviour
 {
@@ -9,6 +10,19 @@ public class pointSystem : MonoBehaviour
 
     List<playerController> playersHere;
     playerController playerHere;
+
+    float p1points = 0;
+    float p2points = 0;
+    float p3points = 0;
+    float p4points = 0;
+
+    float pointMultiplier = 1f;
+
+    [SerializeField] TextMeshProUGUI p1pointText;
+    [SerializeField] TextMeshProUGUI p2pointText;
+    [SerializeField] TextMeshProUGUI p3pointText;
+    [SerializeField] TextMeshProUGUI p4pointText;
+
 
     // Start is called before the first frame update
     void Start()
@@ -39,11 +53,35 @@ public class pointSystem : MonoBehaviour
 
         if (numHere == 1)
         {   //show crown, give points
-
+            switch (playerHere.getPlayerNum())
+            {
+                case 1:
+                    p1points += Time.deltaTime * pointMultiplier;
+                    break;
+                case 2:
+                    p2points += Time.deltaTime * pointMultiplier;
+                    break;
+                case 3:
+                    p3points += Time.deltaTime * pointMultiplier;
+                    break;
+                case 4:
+                    p4points += Time.deltaTime * pointMultiplier;
+                    break;
+                default:
+                    break;
+            }
+            updatePoints();
         }
 
     }
 
+    void updatePoints()
+    {
+        p1pointText.text = ((int)p1points).ToString();
+        p2pointText.text = ((int)p2points).ToString();
+        p3pointText.text = ((int)p3points).ToString();
+        p4pointText.text = ((int)p4points).ToString();
+    }
 
     bool isInTheZone(Transform transf)
     {
