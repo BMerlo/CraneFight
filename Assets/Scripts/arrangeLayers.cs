f24b55c12faa9a5fd72c6ed0aa6fdbcb2f9ed35d
Erdem Gunay <merdemgunay88@gmail.com>
2019-04-08 17:03:19 -0400

    jump first pass

--- Assets/Scripts/arrangeLayers.cs
+++ Assets/Scripts/arrangeLayers.cs
@@ -4,16 +4,34 @@ using UnityEngine;
 
 public class arrangeLayers : MonoBehaviour
 {
+    [SerializeField] GameObject spriteHolder;
+    SpriteRenderer mainSprite;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //mainSprite = transform.Find("Sprite").GetComponent<SpriteRenderer>();
+        if (GetComponent<SpriteRenderer>()) {
+            mainSprite = GetComponent<SpriteRenderer>();
+        }
+        else
+        {
+            mainSprite = spriteHolder.GetComponent<SpriteRenderer>();
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        GetComponent<SpriteRenderer>().sortingOrder = 100 - Mathf.FloorToInt(this.transform.position.y * 10);
+        if (mainSprite)
+        {
+            mainSprite.sortingOrder = 100 - Mathf.FloorToInt(this.transform.position.y * 10);
+        }
+        else
+        {
+            Debug.Log("Wtf");
+        }
+        
 
         Transform SmellCloudF = transform.Find("SmellCloudF");
         Transform smellChildB = transform.Find("SmellCloudB");
@@ -21,15 +39,15 @@ public class arrangeLayers : MonoBehaviour
         //Transform oilChild = transform.Find("OilyPlaceHolder");//Oily Child not implemented on the Player prefab yet.
         if (SmellCloudF != null)
         {
-            SmellCloudF.GetComponent<SpriteRenderer>().sortingOrder = (GetComponent<SpriteRenderer>().sortingOrder) + 5;
+            SmellCloudF.GetComponent<SpriteRenderer>().sortingOrder = (mainSprite.sortingOrder) + 5;
         }
         if (smellChildB != null)
         {
-            smellChildB.GetComponent<SpriteRenderer>().sortingOrder = (GetComponent<SpriteRenderer>().sortingOrder) - 10;
+            smellChildB.GetComponent<SpriteRenderer>().sortingOrder = (mainSprite.sortingOrder) - 10;
         }
         if (oilySprite != null)
         {
-            oilySprite.GetComponent<SpriteRenderer>().sortingOrder = (GetComponent<SpriteRenderer>().sortingOrder) + 1;
+            oilySprite.GetComponent<SpriteRenderer>().sortingOrder = (mainSprite.sortingOrder) + 1;
         }
         // if (oilChild != null)
         // {
