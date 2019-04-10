a622b51802a1e537c55de99e1624c39b8374430e
Erdem Gunay <merdemgunay88@gmail.com>
2019-04-09 22:21:28 -0400

    tank and shadow

---
+++ Assets/TankAI.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankAI : MonoBehaviour
+{
+     ScrollingBackGround backGroundScript;
+     float backgroundSpeed;
+
+    float speed = -1.0f;
+    Vector2 speedVector;
+
+    Rigidbody2D myBody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        backGroundScript = FindObjectOfType<ScrollingBackGround>();
+        backgroundSpeed = backGroundScript.getSpeed();
+
+        speedVector = new Vector2(backgroundSpeed + speed, 0);
+
+        myBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        myBody.velocity = speedVector;
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.transform.GetComponent<destructible>())
+        {
+            collision.transform.GetComponent<destructible>().getDestroyed();
+        }
+    }
+}
