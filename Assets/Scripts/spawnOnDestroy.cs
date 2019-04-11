cf5d203b2127fe4afdf2858d079eef17b0cc7d40
Jepi <extrikey@gmail.com>
2019-04-11 02:06:32 -0400

    Explosions added to car prefabs

---
+++ Assets/Scripts/spawnOnDestroy.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnOnDestroy : MonoBehaviour {
+    [SerializeField] float minForce2Destroy = 0.5f;
+    [SerializeField] GameObject spawnee;
+    [SerializeField] bool doesSpawnOnDestroy = false;
+    [SerializeField] float damage = 5f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        getDestroyed();
+    }
+
+   public void getDestroyed()
+    {
+        if (doesSpawnOnDestroy && spawnee)
+        {
+            Instantiate(spawnee, this.transform.position, this.transform.rotation);
+        }
+        Destroy(this.gameObject, 0.01f);
+    }
+
+
+    public float getDmgAmount()
+    {
+        return damage;
+    }
+}
