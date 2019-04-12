a5bd960002018b8650d2c892715853a85213853e
Jepi <extrikey@gmail.com>
2019-04-11 22:00:25 -0400

    Added Rubble
    Rubble for each car is added. Shadows have not been implemented yet.

--- Assets/Scripts/spawnOnDestroy.cs
+++ Assets/Scripts/spawnOnDestroy.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class spawnOnDestroy : MonoBehaviour {
     [SerializeField] float minForce2Destroy = 0.5f;
-    [SerializeField] GameObject spawnee;
-    [SerializeField] bool doesSpawnOnDestroy = false;
+	[SerializeField] GameObject spawnee;
+	[SerializeField] GameObject spawnee2;
+	[SerializeField] bool doesSpawnOnDestroy = false;
     [SerializeField] float damage = 5f;
 
 	// Use this for initialization
@@ -25,11 +26,15 @@ public class spawnOnDestroy : MonoBehaviour {
 
    public void getDestroyed()
     {
-        if (doesSpawnOnDestroy && spawnee)
-        {
-            Instantiate(spawnee, this.transform.position, this.transform.rotation);
-        }
-        Destroy(this.gameObject, 0.01f);
+		if (doesSpawnOnDestroy && spawnee)
+		{
+			Instantiate(spawnee, this.transform.position, this.transform.rotation);
+		}
+		if (doesSpawnOnDestroy && spawnee2)
+		{
+			Instantiate(spawnee2, this.transform.position, this.transform.rotation);
+		}
+		Destroy(this.gameObject, 0.01f);
     }
 
 
